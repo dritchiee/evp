5de94aa8a50fbd7fb4b63de67434484cd51fa54f
Adrian Kubiński <kubinski.adrian231@gmail.com>
2017-10-05 08:54:51 +0200

    Prepare to run with managed config

--- src/frontends/android/app/src/main/jni/Application.mk
+++ src/frontends/android/app/src/main/jni/Application.mk
@@ -1,3 +1,4 @@
 # select the ABI(s) to build for (see CPU-ARCH-ABIS.html in the NDK docs).
 APP_ABI := armeabi arm64-v8a x86 x86_64 mips mips64
 APP_PLATFORM := android-19
+NDK_TOOLCHAIN_VERSION := 4.9
